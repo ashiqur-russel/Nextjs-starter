6d0d0908fedda405514b9033d2d0f63381cc814e
ashiqurras <ashiqurrussel2@gmail.com>
2022-12-28 00:35:57 +0100

    fetch single data and display

--- pages/blogs/[id].js
+++ pages/blogs/[id].js
@@ -1,7 +1,34 @@
-const Details = () => {
+export const getStaticPaths = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts/");
+  const data = await res.json();
+
+  // map data to an array of path objects with params (id)
+  const paths = data.map((post) => {
+    return {
+      params: { id: post.id.toString() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts/" + id);
+  const data = await res.json();
+
+  return {
+    props: { post: data },
+  };
+};
+const Details = ({ post }) => {
   return (
     <div>
-      <h1>Details page</h1>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
     </div>
   );
 };
