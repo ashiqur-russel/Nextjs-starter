acf196779fb8e96654bdceb62c303d35f33d1005
ashiqurras <ashiqurrussel2@gmail.com>
2022-12-27 23:33:44 +0100

    nextjs data fetching from api using getStaticProps

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,10 @@
+import Image from "next/image";
 import Link from "next/link";
 const Nav = () => {
   return (
     <nav>
       <div className="logo">
-        <h1>My Blog</h1>
+        <Image src="/logo.png" width={77} height={77} alt="logo"></Image>
       </div>
       <div className="nav_link">
         <Link href="/">Home</Link>
--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,17 +1,25 @@
 import React from "react";
 
-const Posts = () => {
+export const getStaticProps = async () => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=10`
+  );
+
+  const data = await response.json();
+  return {
+    props: { posts: data },
+  };
+};
+
+const Posts = ({ posts }) => {
   return (
     <div>
       <h1>All Posts</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
-        praesentium ratione veritatis iste dolorum ipsa?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
-        praesentium ratione veritatis iste dolorum ipsa?
-      </p>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+        </div>
+      ))}
     </div>
   );
 };
