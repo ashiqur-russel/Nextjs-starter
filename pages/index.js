d01dbb4ce862a50e9499542cd29ef32d3d9e4450
ashiqurras <ashiqurrussel2@gmail.com>
2022-12-28 01:18:14 +0100

    footer issue resolved from index

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
 import Link from "next/link";
-import Footer from "../components/footer";
-import Nav from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
